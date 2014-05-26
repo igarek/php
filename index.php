999e837f86b89c20d843f359112cf8366a9ed5df
igarek <igarekkkkk@mail.ru>
2014-05-26 08:55:02 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Students</title>
+</head>
+<body>
+<h1> Student's table</h1>
+<ul>
+  <li><a href="create.php">create table in db</a></li>
+  <li><a href="add.php">add student's data</a></li>
+  <li><a href="list.php">list student's data</a></li>
+</ul>
+</body>
+</html>

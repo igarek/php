53c62b5000873b48c1c6b6c0c343dfca29ff0984
igarek <igarekkkkk@mail.ru>
2014-05-29 09:30:54 +0300

    Update create.php

--- create.php
+++ create.php
@@ -1,21 +1,19 @@
 <?php
-//These are mysql commands for user creation
-//CREATE USER 'student2'@'localhost' IDENTIFIED BY '111';
-//GRANT ALL PRIVILEGES ON * . * TO 'student2'@'localhost';
 
-mysql_connect("localhost","student2","111");
 
-mysql_query("drop database if exists students;");
-mysql_query("create database students;");
-mysql_query("use students;");
-mysql_query("create table students(id int not null auto_increment primary key, 
-	 name char(30), surname char(30), fathersname char(30), age int, 
-	 birthday date, sex char (1));");
-mysql_query("insert into students (name,surname,fathersname,age,birthday,sex) 
-	values ('Maxim', 'Khomenko','Olegovich',19,'1994-09-19','m');");
-mysql_query("insert into students (name,surname,fathersname,age,birthday,sex) 
-	values ('Igor', 'Fedorishin','Evgenievich',20,'1994-03-30','m');");
-mysql_query("insert into students (name,surname,fathersname,age,birthday,sex) 
-	values ('Julia', 'Deyneka','Valeriyevna',20,'1994-04-10','f');");
+mysql_connect("localhost","root2","");
+
+mysql_query("drop database if exists funshop;");
+mysql_query("create database funshop;");
+mysql_query("use funshop;");
+mysql_query("create table funshop(id int not null auto_increment primary key, 
+	 name char(30), brand char(30), producer char(30), quantity int, 
+	 cost int, whom char (1));");
+mysql_query("insert into funshop (name,brand,producer,quantity,cost,whom) 
+	values ('4ashka', 'Premija','Ukraina',5,'120','f');");
+mysql_query("insert into funshop (name,brand,producer,quantity,cost,whom) 
+	values ('4asu', 'Rolix','shwejzarija',5,'1200','m');");
+mysql_query("insert into funshop (name,brand,producer,quantity,cost,whom)  
+	values ('Naushniki', 'Rau-Ban','USA',4,'3000','f,m');");
 echo "Ok.";
 ?>
